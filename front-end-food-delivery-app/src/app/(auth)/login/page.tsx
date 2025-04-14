6dad80fdd03a6217eaa60f13832024632b186bcb
24LP0262 24LP0262 <24LP0262@Setups-iMac.local>
2025-04-14 12:29:33 +0800

    create heseg hiiv

---
+++ front-end-food-delivery-app/src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import {CreateAccount} from "./components/CreateAccount";
+
+export default function Login() {
+  return (
+    <div className="flex">
+      <CreateAccount />
+    </div>
+  );
+}
---
+++ front-end-food-delivery-app/src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="flex h-screen items-center p-20"></div>;
+}
