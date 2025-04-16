9cc18cddeb17f6c7597214a64ad3b2268109bb5a
24LP0262 24LP0262 <24LP0262@Setups-iMac.local>
2025-04-16 12:41:18 +0800

    categories hiij ehlev

--- front-end-food-delivery-app/src/app/(main)/page.tsx
+++ front-end-food-delivery-app/src/app/(main)/page.tsx
@@ -1,10 +1,14 @@
+import {FoodCategoriesList} from "@/mainComponents";
+
 export default function Home() {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen flex-col bg-[#404040] rounded-lg">
       <div className="bg-[#404040] rounded-lg">
         <img src="./Image-bg.png"></img>
       </div>
-      <div></div>
+      <div>
+        <FoodCategoriesList />
+      </div>
     </div>
   );
 }
--- front-end-food-delivery-app/src/mainComponents/elements/FoodCategories.tsx
+++ front-end-food-delivery-app/src/mainComponents/elements/FoodCategories.tsx
@@ -1,5 +1,23 @@
-export const foodCategoriesList = () => {
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export const FoodCategoriesList = () => {
   const foodCategoriesListData = [
+    "Appetizers",
+    "Salads",
+    "Pizzas",
+    "Lunch favorites",
+    "Main dishes",
+    "Fish & Sea foods",
+    "Side dish",
+    "Brunch",
+    "Desserts",
+    "Beverages",
     "Appetizers",
     "Salads",
     "Pizzas",
@@ -12,19 +30,25 @@ export const foodCategoriesList = () => {
     "Beverages",
   ];
   return (
-    <div>
-      <div>
-        <p>Categories</p>
-        {foodCategoriesListData.map((value, index) => {
-          return (
-            <button
-              className="flex size-fit text-black items-center justify-center bg-white"
-              key={index}
-            >
-              {value}
-            </button>
-          );
-        })}
+    <div className="flex p-8 flex-col gap-5 overflow-hidden">
+      <p className="text-white font-semibold text-[30px]">Categories</p>
+      <div className="flex-row flex">
+        <Carousel className="flex absolute">
+          <CarouselContent className="flex relative">
+            {foodCategoriesListData.map((value, index) => {
+              return (
+                <button
+                  className="flex items-center justify-center bg-white border-1 rounded-full min-h-[56px] min-w-[96px] h-fit"
+                  key={index}
+                >
+                  {value}
+                </button>
+              );
+            })}
+          </CarouselContent>
+          <CarouselPrevious className="flex absolute ml-12 size-2 border-none" />
+          <CarouselNext className="flex absolute mr-12 size-2 border-none" />
+        </Carousel>
       </div>
     </div>
   );
